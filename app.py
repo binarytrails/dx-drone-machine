76c7b34d10ecf442f553b01962c84af9468ad2c7
binarytrails <5713600+binarytrails@users.noreply.github.com>
2023-06-25 22:34:53 +0000

    Add missing files to last commit

--- app.py
+++ app.py
@@ -1,33 +1,83 @@
+# Vsevolod Ivanov
+
+import json
 import requests
 
 from flask import Flask, render_template
-from flask import request   
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from geoip2 import database
+from geoip2.errors import AddressNotFoundError
 
 app = Flask(__name__)
 
-users = [{
-    'ip_addr': '39.110.142.79',
-    # latitude,longitude
-    'geo_loc': (35.4594,139.5979)
-}]
+# Define the database location in memory
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-users_geoloc=[]
-users_geoloc.append([-73.778137, 40.641312])
-users_geoloc.append([-0.454296, 51.470020])
-users_geoloc.append([116.597504, 40.072498])
+# Define the schema
+class User(db.Model):
+    ip_address = db.Column(db.String(40), primary_key=True)  # IPV6 address is up to 39 characters + 1 for Null termination
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
 
-@app.route("/")
+    def __init__(self, ip_address, latitude, longitude):
+        self.ip_address = ip_address
+        self.latitude = latitude
+        self.longitude = longitude
+
+# Create the database
+with app.app_context():
+    db.create_all()
+
+# Geolocation database reader
+reader = database.Reader('./databases/GeoLite2-City.mmdb')
+
+@app.route('/geolocations')
+def get_geolocations():
+    # Get distinct latitudes and longitudes from the User table
+    results = db.session.query(User.latitude, User.longitude).distinct().all()
+
+    users_geoloc = []
+    for result in results:
+        users_geoloc.append([result.longitude, result.latitude])
+
+    return {"geolocations": users_geoloc}
+
+@app.route('/')
 def index():
-    #ip_addr = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    print("ip_addr: ", request.remote_addr)
+    ip = request.remote_addr
+    # Get the geolocation data from the IP address
+    try:
+        response = reader.city(ip)
+        latitude = response.location.latitude
+        longitude = response.location.longitude
+    except AddressNotFoundError:
+        if ip == '127.0.0.1':
+            # This is an approximation for Montreal's coordinates
+            latitude = 45.5017
+            longitude = -73.5673
+            print("The IP address {ip} is not a valid GeoLocation. I presume you're in Montreal; bonjour, hi! ;)")
+        else:
+            print('The IP address {ip} is not in the database')
 
-    #response = requests.get(
-    #    "https://geolocation-db.com/json/{ip_addr}&position=true".format(ip_addr='39.110.142.79'),
-    #    timeout=10
-    #).json()
-    # users.append({'ip_addr': request.remote_addr, 'geo_loc': (response['latitude'],response['longitude'])})
+    # Check if user already exists
+    user = User.query.get(ip)
+    if not user:
+        user = User(ip_address=ip, latitude=latitude, longitude=longitude)
+        # Save the data into the database
+        db.session.add(user)
+        db.session.commit()
+        print('Your geolocation has been stored. User IP: {user.ip_address}')
+    else:
+        print('User with IP {ip} already exists')
 
-    return render_template("index.html", title="DX Drone Machine", users_geoloc=users_geoloc)
+    # Convert the list to a JSON formatted string
+    users_geoloc_json = json.dumps({"geolocations": get_geolocations()})
+
+    # Pass the JSON string to the template
+    return render_template('index.html', title="DX Drone Machine", users_geoloc=users_geoloc_json)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=10000)
